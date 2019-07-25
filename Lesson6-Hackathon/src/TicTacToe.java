398a045c398b75cdbd5e7789eeaa327f880a2e4b
Nguyễn Đức Thiên <thiennd@thiennd-macbook-pro.net.fpt>
2019-07-25 20:44:20 +0700

    Re-up Lesson 6

--- Lesson6-Hackathon/src/TicTacToe.java
+++ Lesson6-Hackathon/src/TicTacToe.java
@@ -1,76 +1,78 @@
 import java.util.Scanner;
 
-public class TicTacToe {
-	static Scanner sc = new Scanner(System.in);
+public class TicTacToe { // filename = TicTacToe.java
+	static int x, y;
 	static int[][] board = new int[3][3];
-	static int count = 0;
 
 	public static void main(String[] args) {
-		int x, y;
-
+		int player = 1;
+		int count = 0;
 		while (true) {
-			printBoard(); // print board
-			System.out.print("P1: "); // input x y coordinate  
-			while (true) {
-				x = sc.nextInt();
-				y = sc.nextInt();
-				if (Move(x - 1, y - 1, 1)) // if move success, continue to next turn else repeat 
-					break;
+			System.out.println("Player " + player + "'s turn: ");
+			input();
+			while (check() == false) { // !check()
+				System.out.println("Sai roi, input lai!");
+				input();
 			}
-			count++; // count number of moves. if there are 9 moves => draw 
-			if (count == 9)
-				break;
-			// == //
+			put(player);
 			printBoard();
-			System.out.print("P2: ");
-			while (true) {
-				x = sc.nextInt();
-				y = sc.nextInt();
-				if (Move(x - 1, y - 1, 2))
-					break;
-			}
-			count++;
-			if (count == 9)
+			if(checkWin(player)) break;
+			if (++count > 8) {
+				System.out.println("Draw!");
 				break;
+			}
+			player = 3 - player; // 1 = 3 - 2, 2 = 3 - 1
 		}
-		System.out.println("Draw!");
 	}
 
-	private static boolean Move(int x, int y, int i) {
-		if (board[x][y] == 0) { //  0 => available to move 
-			board[x][y] = i; // flagged that player i move here 
-		} else {
-			System.out.print("P" + i + " wrong move! Again: "); 
-			return false;
+	private static boolean checkWin(int player) { 
+		if (board[0][y] != 0 && board[0][y] == board[1][y] && board[1][y] == board[2][y]) {
+			System.out.println("Player " + player + " win!");
+			return true;
 		}
-		// check all win conditions 
-		if ((board[x][0] == board[x][1] && board[x][2] == board[x][1]) 
-				|| (board[0][y] == board[1][y] && board[1][y] == board[2][y])
-				|| (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] != 0)
-				|| (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] != 0))
-
-		{
-			printBoard();
-			System.out.println("P" + i + " win");
-			System.exit(i);
+		if (board[x][0] != 0 && board[x][0] == board[x][1] && board[x][1] == board[x][2]) {
+			System.out.println("Player " + player + " win!");
+			return true;
 		}
-		return true;
-
+		if (board[0][0] != 0 && board[1][1] == board[0][0] && board[2][2] == board[0][0]) {
+			System.out.println("Player " + player + " win!");
+			return true;
+		}
+		if (board[0][2] != 0 && board[1][1] == board[0][2] && board[1][1] == board[2][0]) {
+			System.out.println("Player " + player + " win!");
+			return true;
+		}
+		return false;
 	}
 
-	// print the current board 
-	static void printBoard() {
+	private static void printBoard() {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
-				if (board[i][j] == 0)
-					System.out.print(" .");
-				if (board[i][j] == 1)
-					System.out.print(" x");
-				if (board[i][j] == 2)
-					System.out.print(" o");
+				System.out.print(board[i][j]);
 			}
 			System.out.println();
 		}
+	}
 
+	private static void put(int player) {
+		board[x][y] = player;
+	}
+
+	private static boolean check() {
+		if (x < 0 || x > 2)
+			return false; // [0,1,2]
+		if (y < 0 || y > 2)
+			return false; // [0,1,2]
+		if (board[x][y] == 0)
+			return true;
+		return false;
+	}
+
+	private static void input() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap x: ");
+		x = sc.nextInt();
+		System.out.print("Nhap y: ");
+		y = sc.nextInt();
 	}
 }
